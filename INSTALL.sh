bf68444f7ef23e0a943dc866c08ad15c1e2018da
Daniel Ranchal <danitico98@gmail.com>
2018-11-24 17:30:08 +0100

    SIFT module added

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,10 +1,10 @@
 # Installing required packages for OpenCV via apt-get
 echo "INSTALLING REQUIRED PACKAGES"
 echo "----------------------------"
-sudo apt-get --yes install build-essential cmake git libgtk2.0-dev 'pkg-config' libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev gcc-multilib g++-multilib
+sudo apt --yes install build-essential cmake git libgtk2.0-dev 'pkg-config' libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev gcc-multilib g++-multilib
 
 if [[ $? > 0 ]]; then
-	echo "It seems that you are having problems with apt-get"
+	echo "It seems that you are having problems with apt"
 	echo "Stopping script ..."
 	exit
 fi
@@ -16,6 +16,7 @@ if [ ! -e /home/$USER/OpenCV/ ]; then
 	mkdir /home/$USER/OpenCV/
 else
 	echo "Folder is already created"
+	rm -rf /home/$USER/OpenCV/*
 fi
 
 # Moving to OpenCV directory
@@ -26,12 +27,13 @@ if [ ! -e opencv.zip ]; then
 	echo "Downloading source code"
 	echo "-----------------------"
 	wget --tries=2 --output-document=opencv.zip -q "https://codeload.github.com/opencv/opencv/zip/3.3.0"
+	wget --tries=2 --output-document=modules.zip -q "https://github.com/opencv/opencv_contrib/archive/3.3.0.zip"
 else
 	find . ! -name 'opencv.zip' -exec rm -rf {} + 2> /dev/null
 fi
 
 # Extracting opencv.zip quietly
-echo "EXTRACTING ZIP"
+echo "EXTRACTING SOURCE CODE"
 echo "--------------"
 unzip -q opencv.zip
 
@@ -41,6 +43,14 @@ mv * ../
 cd ../
 rm -rf opencv-3.3.0/
 
+# Extracting modules.zip quietly
+echo "EXTRACTING CONTRIBUTORS SOURCE CODE"
+echo "------------------"
+unzip -q modules.zip
+
+#Erasing zip file and renaming directory
+mv opencv_contrib-3.3.0/ opencv_contrib/
+
 # Creating temporary directory
 mkdir build
 cd build/
@@ -48,13 +58,13 @@ cd build/
 echo "RUNNING CMAKE"
 echo "-------------"
 # Running Cmake [PARAMETERS*] path_source_code
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTAL_PREFIX=/usr/local ../
+cmake -DCMAKE_BUILD_TYPE=Release -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules -DCMAKE_INSTAL_PREFIX=/usr/local ../
 
 # Once we run cmake, we have the files to build OpenCV. In the directory
 # /opencv/build , we run make using threads.
 echo "BUILDING OPENCV"
 echo "---------------"
-make -j6
+make -j8
 
 # When OpenCV is built, we install the libraries of it
 echo "INSTALLING LIBRARIES"
